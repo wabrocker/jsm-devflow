81c074445dd0daea018b7689775f3f5e076bd1bf
wabrocker <billbrocker@gmail.com>
2025-01-05 17:28:19 -0500

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+
 const Home = () => {
   return (
-    <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next.js</h1>
+    <h1 className="text-3xl font-black text-violet-700">
+      Welcome to the world of Next.js
+    </h1>
   );
-}
+};
 
 export default Home;
