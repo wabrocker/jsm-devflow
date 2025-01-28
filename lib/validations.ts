7b0a4beeadc2569b2cedb489f91c6922b4da2a9a
wabrocker <billbrocker@gmail.com>
2025-01-28 17:04:39 -0500

    Implement the UI of the Question Form

--- lib/validations.ts
+++ lib/validations.ts
@@ -49,3 +49,22 @@ export const SignUpSchema = z.object({
       message: "Username can only contain letters, numbers, and underscores.",
     }),
 });
+
+export const AskQuestionSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Title is required." })
+    .max(100, { message: "Title cannot exceed 100 characters long." }),
+
+  content: z.string().min(100, { message: "Content is required." }),
+
+  tags: z
+    .array(
+      z
+        .string()
+        .min(1, { message: "Tag is required." })
+        .max(30, { message: "Tag cannot exceed 30 characters." })
+    )
+    .min(1, { message: "At least one tag is required." })
+    .max(3, { message: "Cannot add more than 3 tags." }),
+});
