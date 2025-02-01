03dc1676c20bd42d67c3e17c72ad7359fd1b9b2f
wabrocker <billbrocker@gmail.com>
2025-02-01 05:57:43 -0500

    Create User Model

---
+++ database/user.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema } from "mongoose";
+
+export interface User {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const User = models?.User || model<IUser>("User", userSchema);
+
+export default User;
