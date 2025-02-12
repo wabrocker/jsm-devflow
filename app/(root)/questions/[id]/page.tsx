77adad7075a6902a7ddfe4b8b139e7e59a1a506a
wabrocker <billbrocker@gmail.com>
2025-02-12 07:15:05 -0500

    Create Answer Form

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,5 +1,6 @@
 import TagCard from "@/components/cards/TagCard";
 import { Preview } from "@/components/editor/Preview";
+import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
@@ -86,6 +87,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           />
         ))}
       </div>
+
+      <section className="my-5">
+        <AnswerForm questionId={id} />
+      </section>
     </>
   );
 };
