30da55440585e674738290e8e25326237c7fce88
wabrocker <billbrocker@gmail.com>
2025-03-12 16:16:12 -0400

    Implement Get All Users Action

--- types/global.d.ts
+++ types/global.d.ts
@@ -65,3 +65,15 @@ interface Collection {
   question: Question;
   author: string | Author;
 }
+
+interface User {
+  _id: string;
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image?: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
